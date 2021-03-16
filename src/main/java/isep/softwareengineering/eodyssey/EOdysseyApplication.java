28de8aa734c5ec028b26c45fa755c13e2aecfc49
Dragonink <Dragonink@users.noreply.github.com>
2021-03-16 12:45:08 +0100

    Initialize repository

---
+++ src/main/java/isep/softwareengineering/eodyssey/EOdysseyApplication.java
@@ -0,0 +1,13 @@
+package isep.softwareengineering.eodyssey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EOdysseyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EOdysseyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/isep/softwareengineering/eodyssey/EOdysseyApplicationTests.java
@@ -0,0 +1,13 @@
+package isep.softwareengineering.eodyssey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EOdysseyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
