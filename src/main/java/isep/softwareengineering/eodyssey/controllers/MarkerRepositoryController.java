62e9b1002eec817c2d9175d68236538701a8585a
Dragonink <Dragonink@users.noreply.github.com>
2021-05-31 12:06:15 +0200

    Create searchbar

--- src/main/java/isep/softwareengineering/eodyssey/controllers/MarkerRepositoryController.java
+++ src/main/java/isep/softwareengineering/eodyssey/controllers/MarkerRepositoryController.java
@@ -1,9 +1,13 @@
 package isep.softwareengineering.eodyssey.controllers;
 
+import java.util.Optional;
+import java.util.stream.StreamSupport;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import isep.softwareengineering.eodyssey.models.Marker;
@@ -18,5 +22,19 @@ public class MarkerRepositoryController {
 	@GetMapping
 	public @ResponseBody Iterable<Marker> getAll() {
 		return repository.findAll();
+	}
+
+	@GetMapping(path = "/search")
+	public @ResponseBody Iterable<Marker> search(
+		@RequestParam("query") Optional<String> query
+	) {
+		return query
+			.map(str -> {
+				Iterable<Marker> iter = () -> StreamSupport.stream(repository.findAll().spliterator(), false)
+					.filter(marker -> marker.content.contains(str))
+					.iterator();
+				return iter;
+			})
+			.orElseGet(() -> repository.findAll());
 	}
 }
--- src/main/resources/static/scripts/index.js
+++ src/main/resources/static/scripts/index.js
@@ -1,4 +1,15 @@
 const MAP = L.map('map').setView([48.82461184925993, 2.279912667672016], 13);
+const markers = [];
+function setMarkers(newMarkers) {
+	markers.forEach(marker => {
+		marker.remove();//FIXME 'remove' does not work
+	});
+	newMarkers.forEach(marker => void markers.push(
+		L.marker([marker.latitude, marker.longitude])
+			.addTo(MAP)
+			.bindPopup(marker.content)
+	));
+}
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -12,11 +23,21 @@ fetch("/api/markers")
 			throw response;
 		}
 	})
-	.then(markers => {
-		markers.forEach(marker => (
-			L.marker([marker.latitude, marker.longitude])
-				.addTo(MAP)
-				.bindPopup(marker.content)
-		));
-	})
+	.then(setMarkers)
 	.catch(console.error);
+
+((/** @type {HTMLFormElement} */ form) => {
+	form.onsubmit = event => {
+		event.preventDefault();
+		fetch(form.action)
+			.then(response => {
+				if (response.ok) {
+					return response.json();
+				} else {
+					throw response;
+				}
+			})
+			.then(setMarkers)
+			.catch(console.error);
+	};
+})(document.querySelector("form#search"));
